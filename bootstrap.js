c312940a95b8d0fd1754627ee6b47140ed499bdf
Marcus Vinicius Bassalobre de Assis <bassalobre.vinicius@gmail.com>
2022-04-14 09:19:28 -0300

    prevent cors erros

--- bootstrap.js
+++ bootstrap.js
@@ -5,6 +5,14 @@ const http = require('http').createServer(app);
 const cors = require('cors');
 
 app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  next();
+});
+
 const bodyParser = require('body-parser');
 const debug = require('console-development');
 
@@ -18,6 +26,10 @@ app.post('/postback-mockup', async (req, res) => {
   const params = req.body;
   debug.log('postback mockup', params);
   res.json(params);
+});
+
+http.prependListener('request', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
 });
 
 const port = process.env.PORT || 3000;
